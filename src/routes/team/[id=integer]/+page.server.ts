a42d8e34c83324a3cb0537711edd32a21003f49d
lj-n <linus.johannsen@outlook.com>
2023-10-04 22:21:27 +0200

    add user invite form

--- src/routes/team/[id=integer]/+page.server.ts
+++ src/routes/team/[id=integer]/+page.server.ts
@@ -1,7 +1,8 @@
-import { db, protectRoute, schema } from '$lib/server';
+import { db, protectRoute, schema, sendMail } from '$lib/server';
 import { and, eq, sql } from 'drizzle-orm';
-import type { PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { error, fail } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 
 const getTeamMembers = db
 	.select({
@@ -34,7 +35,81 @@ export const load: PageServerLoad = async ({ locals, params }) => {
 	}
 
 	const team = await getTeamMembers.all({ teamId });
-	
 
 	return { team };
 };
+
+export const actions = {
+	usersearch: async ({ locals, request }) => {
+		const session = await locals.auth.validate();
+		if (!session) return fail(401, { error: 'Unauthorized' });
+
+		const formData = await request.formData();
+		const email = formData.get('email')?.toString();
+
+		if (!email) return fail(400, { error: 'Missing email' });
+
+		const [user] = await db
+			.select()
+			.from(schema.user)
+			.where(eq(schema.user.email, email));
+
+		if (!user) return fail(400, { error: 'User not found' });
+
+		return { foundUser: user };
+	},
+
+	userinvite: async ({ locals, request, params }) => {
+		const session = await locals.auth.validate();
+		if (!session) return fail(401, { error: 'Unauthorized' });
+
+		const formData = await request.formData();
+		const userId = formData.get('userId')?.toString();
+
+		if (!userId) return fail(401, { error: 'Missing user id' });
+
+		try {
+			const invitedUser = await db.transaction(async (tx) => {
+				const [foundUser] = await tx
+					.select()
+					.from(schema.user)
+					.where(eq(schema.user.id, userId));
+
+				if (!foundUser) {
+					throw new Error('User not found');
+				}
+
+				await tx.insert(schema.teamMember).values({
+					role_id: 3,
+					team_id: Number(params.id),
+					user_id: foundUser.id
+				});
+
+				return foundUser;
+			});
+
+			const base = dev
+				? 'http://localhost:5173'
+				: 'https://genug-sveltekit.fly.dev';
+			const url = new URL(`/team/${params.id}`, base);
+
+			const html = `
+				<p>Hi ${invitedUser.name} 👋,</p>
+				<p>${session.user.name} invited you to a team</p>
+				<a href="${url.href}">Click here to go to the team page</a>
+			`;
+
+			await sendMail({
+				from: '"genug.app" <no-reply@genug.app>',
+				to: invitedUser.email,
+				subject: 'You got invited to join a team',
+				html
+			});
+
+			return { success: true };
+		} catch (error) {
+			console.log('🛸 < file: +page.server.ts:82 < error =', error);
+			return fail(500, { error: 'Something went wrong, please try again.' });
+		}
+	}
+} satisfies Actions;
--- src/routes/team/[id=integer]/+page.svelte
+++ src/routes/team/[id=integer]/+page.svelte
@@ -1,9 +1,55 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+	import { error } from '@sveltejs/kit';
+	import type { ActionData, PageData } from './$types';
 
 	export let data: PageData;
+	export let form: ActionData;
 </script>
 
-<h1>Team page</h1>
+<main class="w-fit flex flex-col gap-4">
+	<h1>Team page</h1>
 
-<pre>{JSON.stringify(data, null, 2)}</pre>
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+
+	<form method="POST" action="?/usersearch" use:enhance>
+		<div class="form-control w-full max-w-xs">
+			<label class="label" for="email">
+				<span class="label-text">Invite user per email</span>
+			</label>
+
+			<div class="join">
+				<input
+					class="input input-sm input-bordered join-item"
+					type="text"
+					placeholder="type email here"
+					id="email"
+					name="email"
+				/>
+				<button type="submit" class="btn btn-sm btn-accent join-item">
+					search users
+				</button>
+			</div>
+		</div>
+	</form>
+
+	{#if form?.error}
+		<p>{form.error}</p>
+	{/if}
+
+	{#if form?.foundUser}
+		<form method="POST" action="?/userinvite" use:enhance>
+			<input
+				type="hidden"
+				name="userId"
+				id="userId"
+				value={form.foundUser.id}
+			/>
+
+			<div class="flex justify-between">
+				<span>{form.foundUser.name}</span>
+				<button type="submit" class="btn btn-sm">Invite User</button>
+			</div>
+		</form>
+	{/if}
+</main>
