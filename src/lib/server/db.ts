e2836e9fb75940cd5afd3ffc6e4729edb48598eb
lj-n <linus.johannsen@outlook.com>
2023-12-21 00:37:38 +0100

    remove automatic db migration from

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,7 +4,6 @@ import {
 } from 'drizzle-orm/better-sqlite3';
 import SQLiteDatabase from 'better-sqlite3';
 import { schema } from '$lib/server/schema';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 export type Database = BetterSQLite3Database<typeof schema>;
 
@@ -14,5 +13,3 @@ export const db: Database = drizzle(sqlite, {
 	schema,
 	logger: false
 });
-
-migrate(db, { migrationsFolder: 'migrations' });
