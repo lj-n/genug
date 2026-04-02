ee1b34c1abca5ac2ddcfb5b40f5d9a2bd1223ba6
lj-n <linus.johannsen@outlook.com>
2026-04-02 16:41:34 +0200

    wip setup

--- src/lib/components/ui/input-currency/input-currency.svelte
+++ src/lib/components/ui/input-currency/input-currency.svelte
@@ -90,7 +90,7 @@
 	value={internalValue}
 	data-slot={dataSlot}
 	class={cn(
-		'h-9 w-full rounded-md border border-muted/30 bg-surface px-3 py-1 outline-none placeholder:text-muted/90 focus-visible:ring-2 focus-visible:ring-focus/80 aria-invalid:border-error',
+		'h-9 w-full rounded-md border border-muted/30 bg-focus-muted px-3 py-1 outline-none placeholder:text-muted/90 focus-visible:ring-2 focus-visible:ring-focus/80 aria-invalid:border-error',
 		className
 	)}
 	{intlConfig}
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
 	import { InputCurrency } from '$lib/components/ui/input-currency';
 
 	let value = $state(24.99);
 </script>
 
-<div class="m-auto max-w-lg">
-	<pre>{JSON.stringify(value, null, 2)}</pre>
-	<InputCurrency
-		aria-label="Amount"
-		name="amount"
-		intlConfig={{ currency: 'EUR', locale: 'de-DE' }}
-		bind:value
-	/>
+<InputCurrency
+	class="m-auto max-w-lg"
+	aria-label="Amount"
+	name="amount"
+	intlConfig={{ currency: 'EUR', locale: 'de-DE' }}
+	bind:value
+/>
+<div class="m-auto max-w-lg rounded-md border border-muted/10 bg-surface p-8 shadow-md">
+	<h1 class="text-2xl font-bold tracking-tighter">Currency Input Example</h1>
+	<pre class="my-8">{JSON.stringify(value, null, 2)}</pre>
 
 	<InputCurrency
 		aria-label="Amount with prefix and suffix"
@@ -19,4 +22,9 @@
 		intlConfig={{ currency: 'USD', locale: 'en-US' }}
 		bind:value
 	/>
+
+	<div class="mt-6 flex justify-between">
+		<Button variant="ghost">Cancel</Button>
+		<Button>Save</Button>
+	</div>
 </div>
